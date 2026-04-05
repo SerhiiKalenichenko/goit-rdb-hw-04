85cef1fabab15c703f850f4e2b9ac688b10efaaf
SerhiiKalenichenko <serhiy.kalenichenko@gmail.com>
2026-04-05 22:53:48 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -136,3 +136,16 @@ INNER JOIN categories cat ON p.category_id = cat.id
 INNER JOIN employees e ON o.employee_id = e.employee_id
 INNER JOIN shippers s ON o.shipper_id = s.id
 INNER JOIN suppliers sup ON p.supplier_id = sup.id;
+
+-- Task 4.3 FILTER
+
+SELECT COUNT(*) AS total_rows
+FROM order_details od
+INNER JOIN orders o ON od.order_id = o.id
+INNER JOIN customers c ON o.customer_id = c.id
+INNER JOIN products p ON od.product_id = p.id
+INNER JOIN categories cat ON p.category_id = cat.id
+INNER JOIN employees e ON o.employee_id = e.employee_id
+INNER JOIN shippers s ON o.shipper_id = s.id
+INNER JOIN suppliers sup ON p.supplier_id = sup.id
+WHERE e.employee_id > 3 AND e.employee_id <= 10;
