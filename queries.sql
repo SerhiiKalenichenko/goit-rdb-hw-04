67c4bab3f49b79d6dfadfabb7180b02413c1594c
SerhiiKalenichenko <serhiy.kalenichenko@gmail.com>
2026-04-05 22:40:46 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -124,3 +124,15 @@ LEFT JOIN categories cat ON p.category_id = cat.id
 INNER JOIN employees e ON o.employee_id = e.employee_id
 INNER JOIN shippers s ON o.shipper_id = s.id
 INNER JOIN suppliers sup ON p.supplier_id = sup.id;
+
+-- Task 4.2 RIGHT JOIN
+
+SELECT COUNT(*) AS total_rows
+FROM order_details od
+RIGHT JOIN orders o ON od.order_id = o.id
+INNER JOIN customers c ON o.customer_id = c.id
+INNER JOIN products p ON od.product_id = p.id
+INNER JOIN categories cat ON p.category_id = cat.id
+INNER JOIN employees e ON o.employee_id = e.employee_id
+INNER JOIN shippers s ON o.shipper_id = s.id
+INNER JOIN suppliers sup ON p.supplier_id = sup.id;
