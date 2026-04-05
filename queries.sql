f2554f0342cee31594a43c6ca67132a1fc9ae385
SerhiiKalenichenko <serhiy.kalenichenko@gmail.com>
2026-04-05 23:25:41 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -165,3 +165,21 @@ INNER JOIN shippers s ON o.shipper_id = s.id
 INNER JOIN suppliers sup ON p.supplier_id = sup.id
 WHERE e.employee_id > 3 AND e.employee_id <= 10
 GROUP BY cat.name;
+
+-- Task 4.5 GROUP BY + AVG
+
+SELECT 
+    cat.name AS category_name,
+    COUNT(*) AS total_orders,
+    AVG(od.quantity) AS avg_quantity
+FROM order_details od
+INNER JOIN orders o ON od.order_id = o.id
+INNER JOIN customers c ON o.customer_id = c.id
+INNER JOIN products p ON od.product_id = p.id
+INNER JOIN categories cat ON p.category_id = cat.id
+INNER JOIN employees e ON o.employee_id = e.employee_id
+INNER JOIN shippers s ON o.shipper_id = s.id
+INNER JOIN suppliers sup ON p.supplier_id = sup.id
+WHERE e.employee_id > 3 AND e.employee_id <= 10
+GROUP BY cat.name
+HAVING AVG(od.quantity) > 21;
